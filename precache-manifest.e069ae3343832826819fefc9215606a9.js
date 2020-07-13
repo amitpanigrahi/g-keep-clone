a458eedef8c1c95b62f40f28e8fa81fc825b2222
amitx <amitbizweb@gmail.com>
2020-07-13 18:05:43 +0530

    Updates

---
+++ precache-manifest.e069ae3343832826819fefc9215606a9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fa44c5ec98c704521d787cdbe1e7d0",
+    "url": "/g-keep-clone/index.html"
+  },
+  {
+    "revision": "44943af2b2f4e5ca3ba0",
+    "url": "/g-keep-clone/static/js/2.6774d63b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/g-keep-clone/static/js/2.6774d63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa21d04080c2b559d31",
+    "url": "/g-keep-clone/static/js/main.e432bac1.chunk.js"
+  },
+  {
+    "revision": "5ab5e9d50f0501b1f224",
+    "url": "/g-keep-clone/static/js/runtime-main.6e19bb4d.js"
+  },
+  {
+    "revision": "dc8e306a30eaae0f0eb204080b095134",
+    "url": "/g-keep-clone/static/media/archive.dc8e306a.svg"
+  },
+  {
+    "revision": "35ec9087eb4760f64db4352e5287323d",
+    "url": "/g-keep-clone/static/media/close.35ec9087.svg"
+  },
+  {
+    "revision": "0b7a3fe792a9b479ccf2001307e5fd66",
+    "url": "/g-keep-clone/static/media/moon.0b7a3fe7.svg"
+  },
+  {
+    "revision": "6a27d4aaa00a3285a69d02eaaa6329ad",
+    "url": "/g-keep-clone/static/media/notes.6a27d4aa.svg"
+  },
+  {
+    "revision": "bb1249bf3c920f4dabc73635514ba712",
+    "url": "/g-keep-clone/static/media/pin.bb1249bf.svg"
+  },
+  {
+    "revision": "ef16b375e8435709ea23a27847cb576e",
+    "url": "/g-keep-clone/static/media/pinned.ef16b375.svg"
+  },
+  {
+    "revision": "842f85509ce3f7b9f74cb6f7494449ff",
+    "url": "/g-keep-clone/static/media/search.842f8550.svg"
+  },
+  {
+    "revision": "112314e914446a249c3a6a5a1451ecd4",
+    "url": "/g-keep-clone/static/media/sun.112314e9.svg"
+  },
+  {
+    "revision": "f350b5318a66d5d3316de53aa42d0500",
+    "url": "/g-keep-clone/static/media/trash.f350b531.svg"
+  },
+  {
+    "revision": "c7a724873c481b17eda37fc470dfe613",
+    "url": "/g-keep-clone/static/media/trashRestore.c7a72487.svg"
+  },
+  {
+    "revision": "2991c2656fcd8c53f2ff4100dd26d247",
+    "url": "/g-keep-clone/static/media/unarchive.2991c265.svg"
+  }
+]);
