2b4c17d7e51be6430ecbb54826297bcde5307c54
amitx <amitbizweb@gmail.com>
2020-07-13 17:51:42 +0530

    Removed dev enhancers from store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,5 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore} from 'redux';
 import {rootReducer} from "../reducers";
-import logger from "redux-logger";
-import {composeWithDevTools} from "redux-devtools-extension/developmentOnly";
 import {loadState, saveState} from "../utils/localStorage";
 import {debounce} from "../utils/helper";
 
@@ -9,20 +7,16 @@ import {debounce} from "../utils/helper";
 export const configureStore = () => {
 
     const persistedState = loadState();
-    const middlewareEnhancer = applyMiddleware(logger);
-    const enhancers = [middlewareEnhancer];
-    const composedEnhancers = composeWithDevTools(...enhancers);
 
     const store = createStore(
         rootReducer,
-        persistedState,
-        composedEnhancers);
+        persistedState);
 
     store.subscribe(debounce(() => {
         saveState({
             ...store.getState()
         });
-    }, 5000));
+    }, 3000));
 
     return store
 };
